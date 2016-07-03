3d2e8d8c4169d95e945ee120f55c4889b112666b
Jos Verlinde <jos_verlinde@hotmail.com>
2016-07-04 00:13:42 +0200

    Add Initial tests for Module

---
+++ AADGraph-Module.Tests.ps1
@@ -0,0 +1,15 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+
+#Handle Module Testing Module 
+$sut = $sut -replace '-Module', ''
+$sut = $sut -replace '.ps1', ''
+
+$VerbosePreference = 2
+Import-Module -FullyQualifiedName "$here\$sut"
+
+Describe "AADGraph-Module" {
+    It "does something useful" {
+        $true | Should Be $false
+    }
+}
