3d37cff16de1b9ea85a9fa2293a67c5bacc90647
Jos Verlinde <jos_verlinde@hotmail.com>
2016-07-04 00:48:20 +0200

    add more tests

--- AADGraph-Module.Tests.ps1
+++ AADGraph-Module.Tests.ps1
@@ -3,13 +3,42 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
 #Handle Module Testing Module 
 $sut = $sut -replace '-Module', ''
-$sut = $sut -replace '.ps1', ''
+$sut = $sut -replace '.ps1', '.psd1'
 
-$VerbosePreference = 2
+Write-Host "Test Module : $here\$sut" 
+$VerbosePreference = 0
+
 Import-Module -FullyQualifiedName "$here\$sut"
 
 Describe "AADGraph-Module" {
-    It "does something useful" {
-        $true | Should Be $false
+
+    it "has a valid Module Manifest" {
+        {$Mod = Test-ModuleManifest "$here\$sut" } | Should Not Throw 
+        $mod.Name | Should be "AADGraph"
+
+        $mod.ExportedCmdlets.Count | Should be 0
     }
+    it "has Exported Function" {
+        $mod.ExportedFunctions.Count | Should be 15 
+    }
+    it "has nested modules" {
+ 
+        $mod.NestedModules.Count  | Should  be 0
+    }
+
+    it "Installs Nuget" -Pending {
+    }
+
+    it "Downloads the ADAL Libraries" -Pending {
+    }
+
+    it "Can Load the ADAL Libraries" -Pending {
+    }
+
+    it "Can remove NUGET and the ADAL Libraries" -Pending {
+    }
+
+
 }
+
+
